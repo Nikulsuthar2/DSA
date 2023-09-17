b4d1459628dc881a24d7b796481ae8d32a53c7d7
Nikul Suthar <nikulsuthar973@gmail.com>
2023-09-17 21:48:06 +0530

    dsa started

---
+++ C++/Array/initializeArray.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void printArray(int arr[], int size){
+    cout<<"Array : ";
+    for(int i=0; i<size; i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+void getArray(int arr[], int size){
+    cout<<"Enter Array : ";
+    for(int i=0; i<size; i++){
+        cin>>arr[i];
+    }
+}
+
+int main(){
+    int arr[5];
+
+    getArray(arr, 5);
+    printArray(arr, 5);
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# DSA
+## C++
+### 1. Array
+- Initialize Array
+- Find Maximum and Minimum
